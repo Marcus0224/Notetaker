9749a29e0ae07c3a4973f28e1b24da9ed222786f
Marcus0224 <marcstlouis04@gmail.com>
2022-03-26 20:55:28 -0400

    update PORT

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 // Listener
-app.listen(3001, () => {
-    console.log (`API server now on port 3001`);
+app.listen(PORT, () => {
+    console.log (`API server now on port ${PORT}!`);
 });
